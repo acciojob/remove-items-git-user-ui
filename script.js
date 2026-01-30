779ccb80a81623a2bf6dfd329b8464e251a68e3a
git-user-ui <pratikpp2302@gmail.com>
2026-01-30 19:46:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeColor() {
+    const select = document.getElementById("colorSelect");
+
+    // Remove the currently selected option
+    select.remove(select.selectedIndex);
+}
